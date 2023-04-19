4e54ecfea332bb8c42397cfaa621b65a86fe494a
Danya <Mochalovdy@yandex.ru>
2023-04-20 01:28:59 +0300

    the first major update!
    changed the logic of the site, added local storage, put protection against re-entry.

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const form = document.getElementById('form');
-const teams = ['Государственные перфекционисты', 'Департамент дел и докладов', 'Казначеи и казначейства', 'Бюрократические братья', 'государственные шаманы', 'Департамент дерзости и душевности', 'Команда бюрократических гуру'];
+const teams = [
+  { name: "Государственные перфекционисты", link: "https://vk.me/join/vyfPk6Q1apqAcEeXZCxMJtgCvsIrIyhHwDg=" },
+  { name: "Департамент дел и докладов", link: "https://vk.me/join/CCeLXOLzHkpVbFfk_ZXxQvB_KL/o95OKSPU=" },
+  { name: "Казначеи и казначейства", link: "https://vk.me/join/nsXNnkIuDe8kadzh8UaHH3iTMPxmcmGTDKk=" },
+  { name: "государственные шаманы", link: "https://vk.me/join/qFoeCD573IuJXYdVnsL5XGLq2cS4Eq2iDrw=" },
+  { name: "Департамент дерзости и душевности", link: "https://vk.me/join/cEyPtWLhkuaMCzeIKymnTlMkbVW5FV/cPmA=" },
+];
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -7,12 +13,24 @@ form.addEventListener('submit', function(event) {
     const surnameInput = document.getElementById('surname');
     const name = nameInput.value;
     const surname = surnameInput.value;
+  
+    let assignedTeam = JSON.parse(localStorage.getItem('assignedTeam'));
+    if (assignedTeam) {
+      const resultElement = document.getElementById('result');
+      resultElement.innerHTML = `Вы уже были назначены в команду <a href="${assignedTeam.link}">${assignedTeam.name}</a>`;
+      form.style.display = 'none';
+      resultElement.style.display = 'block';
+      return;
+    }
+  
     const team = teams[Math.floor(Math.random() * teams.length)];
+    localStorage.setItem('assignedTeam', JSON.stringify(team));
+  
     const resultElement = document.getElementById('result');
-    resultElement.textContent = `${surname} ${name}, вы назначены в ${team}`;
+    resultElement.innerHTML = `${surname} ${name}, вы назначены в команду <a href="${team.link}">${team.name}</a>`;
     form.style.display = 'none';
     resultElement.style.display = 'block';
-});
+  });
 
 
 
